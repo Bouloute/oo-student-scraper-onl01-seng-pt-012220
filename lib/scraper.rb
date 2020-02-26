98c7b5befa7a644fbadb91927c8f1b7dcf9ce663
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-26 00:58:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    students = {}
     scraped = Nokogiri::HTML(open(index_url))
     student_cards = scraped.css(".student-card")
+    #student_cards[0].css(".student-name").text == "Ryan Johnson"
+    student_cards.each {|student|
+      
+    }
     binding.pry
   end
 
