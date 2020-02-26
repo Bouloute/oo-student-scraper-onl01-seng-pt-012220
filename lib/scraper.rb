9cad306bb6565e357221f774a20f8ff6780b9c1a
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-26 00:47:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     scraped = Nokogiri::HTML(open(index_url))
     student_cards = scraped.css(".student-card")
-
+    binding.pry 
   end
 
   def self.scrape_profile_page(profile_url)
