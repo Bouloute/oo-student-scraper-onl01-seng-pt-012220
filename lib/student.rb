59efedd5a3197f57a54eb04e75a68b0c6b8b819e
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-26 02:53:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @name = student_hash[:name] if !student_hash[:name] #, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+    @name = student_hash[:name] if student_hash[:name] #, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
   end
 
   def self.create_from_collection(students_array)
