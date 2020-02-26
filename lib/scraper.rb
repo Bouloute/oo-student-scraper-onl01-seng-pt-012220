58e097136b0e1b991f695ebc92f3cbb48ce3342e
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-26 02:41:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,6 +25,19 @@ class Scraper
     student_data[:bio] = student.css(".description-holder").css("p").text
     student_data[:profile_quote] = student.css(".profile-quote").text
     #student_data[:blog] = student.css(".profile-quote").text
+
+    binding.pry
+    student.css("social-icon-container").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
+    end
     student_data
   end
 
