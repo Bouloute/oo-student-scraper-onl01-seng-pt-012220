df59d2e409fd84efe4ca60b0dc4e584b5c9e3603
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-26 03:07:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,7 @@ class Student
     @profile_quote = student_hash[:profile_quote] if student_hash[:profile_quote]
     @bio = student_hash[:bio] if student_hash[:bio]
     @profile_url = student_hash[:profile_url] if student_hash[:profile_url]
+    
     @@all.push(self)
   end
 
@@ -22,7 +23,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+binding.pry
   end
 
   def self.all
